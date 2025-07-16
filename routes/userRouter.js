da9f24c24d4c8e370437c2f93ad58043bcfe6917
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-07-16 08:44:45 +0530

    Add user registration route and index route setup

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require('../models/usermodels');
 
 router.get("/", (req, res) => {
     res.send("user route is working now!");
 });
+
+router.post("/register", async (req, res) => {
+    try {
+        let { fullName, email, password, location, contactNo } = req.body;
+
+        // console.log(fullName, email, password, location, contactNo);
+
+        let user = await userModel.create({
+            fullName,
+            email,
+            password,
+            location,
+            contactNo
+        });
+        console.log(user);
+        res.send(user);
+        // res.redirect("/");
+    }
+    catch (err)  {
+    console.log(err.message);
+    // res.redirect("/");
+    }
+})
 
 
 module.exports = router;
