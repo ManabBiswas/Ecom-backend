0d478507bca17c5fe351e574bb90e2c298288fe1
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-10-04 13:38:26 +0530

    add engin

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
